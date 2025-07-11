f4f01372e1fef6f2d920dc60d35431f36f2df2be
Kamushek <strmitwhatilike@gmail.com>
2025-07-11 14:59:36 +0300

    moved the styles to another file and changed the colors according to the css-variables

--- src/features/tasks-column/tasks-column.tsx
+++ src/features/tasks-column/tasks-column.tsx
@@ -13,84 +13,42 @@ type TasksColumnProps = {
 };
 
 const TasksColumn: React.FC<TasksColumnProps> = ({ title, tasks, status }) => {
-  /* 
-    TODO: 
-    1. Такие функции в начале лучше выносить в отдельные файлы, например, в shared/lib/helpers,
-    в компоненте это занимает много места и создает шум
-
-    2. Для задания цвета лучше использовать css-переменные,
-    посмотри в app/styles/index.scss какие переменные у меня есть
-
-    3. Ты прописываешь одинаковые стили для каждой колонки и меняешь только цвета,
-    лучше объединить одинаковую часть и взаимодействовать только с цветом
-    
-    4. Наш код стайл - стили и компоненты в разных файлах,
-    стили лучше вынести в tasks-column.module.scss, и вот эти функции в начале
-    лучше тоже на ксс классы перенести
-
-    5. Задать максимальную высоту колонки и сделать возможность скролла
-
-    Лично я бы вынес стили в другой файл, прописал бы классы для колонки, заголовка и т.д.,
-    и отдельными css-классами прописал бы цвета и с помощью clsx давал бы дополнительный класс цвета для колонки. 
-    Ну впринципе я сейчас так и сделал, можешь на это ориентироваться
-  */
-
-  const getColumnStyle = () => {
+  const getColumnColorClass = () => {
     switch (title) {
       case "Открыто":
-        return {
-          width: 368,
-          height: 53,
-          border: "2px solid #FFAF7D",
-          background: "#FFFFFF",
-          borderRadius: 16,
-        };
+        return styles["column--open"];
       case "В процессе":
-        return {
-          width: 368,
-          height: 53,
-          border: "2px solid #7D97FF",
-          background: "#FFFFFF",
-          borderRadius: 16,
-        };
+        return styles["column--in-progress"];
       case "Завершено":
-        return {
-          width: 368,
-          height: 53,
-          border: "2px solid #00BD7B",
-          background: "#FFFFFF",
-          borderRadius: 16,
-        };
+        return styles["column--done"];
       default:
-        return {};
+        return "";
     }
   };
 
-  const getTitleColor = () => {
+  const getTitleColorClass = () => {
     switch (title) {
       case "Открыто":
-        return "#FFAF7D";
+        return styles["title--open"];
       case "В процессе":
-        return "#7D97FF";
+        return styles["title--in-progress"];
       case "Завершено":
-        return "#00BD7B";
+        return styles["title--done"];
       default:
-        return "#000000";
+        return "";
     }
   };
 
   return (
-    <div>
-      <div style={getColumnStyle()} className={styles.column__title}>
-        <div>{taskStatusIcon[status]}</div>
-        <p style={{ color: getTitleColor(), fontSize: 20, fontWeight: 400 }}>
-          {title}
-        </p>
+    <div className={styles.wrapper}>
+      <div className={clsx(styles.title, getTitleColorClass())}>
+        <div className={styles.title__icon}>{taskStatusIcon[status]}</div>
+        <p className={styles.title__text}>{title}</p>
       </div>
       <Droppable droppableId={status}>
         {(provided) => (
           <div
-            className={clsx(styles.column, styles[`column-${status}`])}
+            className={clsx(styles.column, styles[`column-${status}`], getColumnColorClass())}
             {...provided.droppableProps}
             ref={provided.innerRef}
           >
